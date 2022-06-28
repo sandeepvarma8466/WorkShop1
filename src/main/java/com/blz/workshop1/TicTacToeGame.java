78a1f94b83740c8f083be9232febb6543f88aa3e
sandeepvarma8466 <svarma852@gmail.com>
2022-06-28 17:20:30 +0530

    move to a desired location

--- src/main/java/com/blz/workshop1/TicTacToeGame.java
+++ src/main/java/com/blz/workshop1/TicTacToeGame.java
@@ -3,6 +3,7 @@ package com.blz.workshop1;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+    static Scanner sc = new Scanner(System.in);
     static char[] board = new char[10];
     static int i;
     public static char[] createBoard() {
@@ -15,7 +16,7 @@ public class TicTacToeGame {
         char player = ' ';
         char computer = ' ';
         System.out.println("Enter the Letter X or O");
-        Scanner sc = new Scanner(System.in);
+
         char letter = sc.next().charAt(0);
         if (letter == 'X') {
             player = letter;
@@ -43,6 +44,41 @@ public class TicTacToeGame {
         System.out.println("Welcome to TicTacToe game");
         createBoard();
         chooseLetter();
+        showBoard();
+        System.out.println("\nSelect the position to in board from range [0 - 8] : ");
+        int playerSelect = sc.nextInt();
+        char playerSymbol = 0;
+        switch (playerSelect) {
+            case 0: if (board[0] == ' ')
+                board[0] = playerSymbol;
+                break;
+            case 1: if (board[1] == ' ')
+                board[1] = playerSymbol;
+                break;
+            case 2: if (board[2] == ' ')
+                board[2] = playerSymbol;
+                break;
+            case 3: if (board[3] == ' ')
+                board[3] = playerSymbol;
+                break;
+            case 4: if (board[4] == ' ')
+                board[4] = playerSymbol;
+                break;
+            case 5: if (board[5] == ' ')
+                board[5] = playerSymbol;
+                break;
+            case 6: if (board[6] == ' ')
+                board[6] = playerSymbol;
+                break;
+            case 7: if (board[7] == ' ')
+                board[7] = playerSymbol;
+                break;
+            case 8: if (board[8] == ' ')
+                board[8] = playerSymbol;
+                break;
+            default:
+                System.out.println("Invalid Position ! Restart game");
+        }
         showBoard();
     }
 }
