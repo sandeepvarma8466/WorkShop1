1231554e9f2ad070fef24a15d9c82cce033fbbca
sandeepvarma8466 <svarma852@gmail.com>
2022-06-28 16:48:59 +0530

    player to choose a letter

--- src/main/java/com/blz/workshop1/TicTacToeGame.java
+++ src/main/java/com/blz/workshop1/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.blz.workshop1;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     static char[] board = new char[10];
     static int i;
@@ -9,8 +11,31 @@ public class TicTacToeGame {
         }
         return board;
     }
+    public static void chooseLetter(){
+        char player = ' ';
+        char computer = ' ';
+        System.out.println("Enter the Letter X or O");
+        Scanner sc = new Scanner(System.in);
+        char letter = sc.next().charAt(0);
+        if (letter == 'X') {
+            player = letter;
+            computer = 'O';
+            System.out.println("Player's letter is X");
+            System.out.println("Computer's letter is O");
+        }
+        else if (letter == 'O'){
+            player = 'O';
+            computer = 'X';
+            System.out.println("Player's letter is O");
+            System.out.println("Computer's letter is X");
+        }
+        else {
+            System.out.println("Error.You have to choose X or O only");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe game");
         System.out.println(ticTac());
+        chooseLetter();
     }
 }
